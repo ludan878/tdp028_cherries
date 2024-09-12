f0593d0d9a0c1424876f5d175e4048ef1190fa10
Louumi Anderstedt <ludan878@student.liu.se>
2024-09-12 16:51:43 +0200

    Milestone 2

---
+++ testing/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Star',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  NetworkImage _image = NetworkImage('https://picsum.photos/200/300');
+
+  void _buttonPress() {
+    setState(() {
+      _counter++;
+      _image = NetworkImage('https://picsum.photos/200/300?random=$_counter');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: _image,
+            ),
+            Text(
+              'Random Image Generator',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _buttonPress,
+        tooltip: 'Press to see something cool!',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
